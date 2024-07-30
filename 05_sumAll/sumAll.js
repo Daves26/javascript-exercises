df7596ac0714c15115d5c6c32fb2da8e610aa83f
Daves26 <juandavesga8@gmail.com>
2024-07-30 14:36:29 -0500

    Solved sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let sum = 0;
+
+    if (first > second) {
+        let temp = first;
+        first = second;
+        second = temp;
+    } else if (first < 0 || second < 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(first) || !Number.isInteger(second)) {
+        return "ERROR";
+    } 
+
+    for (let i = first; i <= second; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
