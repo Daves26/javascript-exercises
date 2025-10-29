93b14830e6e174d9d89268da4ed81b5bed4f9445
Daves26 <juandavesga8@gmail.com>
2025-10-28 21:39:17 -0500

    solved removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
 const removeFromArray = function() {
+    if (arguments.length == 1) {
+        return arguments[0];
+    }
+
+    let args = Array.from(arguments);
+
+    function isNotAnArg(item) {
+        if (args.lastIndexOf(item) == -1) {
+            return true;
+        }
+        return false;
+    }
+
+    const answer = args[0].filter(isNotAnArg);
+    //console.log(answer);
+    return answer;
 };
 
+//removeFromArray([1, 2, 3, 4], 4, 3);
 // Do not edit below this line
 module.exports = removeFromArray;
