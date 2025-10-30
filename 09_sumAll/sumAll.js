9409f26b71764b46b98fd8ec27addc87fb21bbf3
Daves26 <juandavesga8@gmail.com>
2025-10-29 19:58:33 -0500

    solved sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
 const sumAll = function() {
 
+    if (arguments[0] < 0 || arguments[1] < 0 ||
+        typeof arguments[0] != 'number' || typeof arguments[1] != 'number' ||
+        arguments[0] % 1 != 0 || arguments[1] % 1 != 0
+    ) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    if (arguments[0] < arguments[1]) {
+        const first = arguments[0];
+        const second = arguments[1];
+        for (let i = first; i <= second; i++) {
+            sum += i;
+        }
+    } else {
+        const first = arguments[1];
+        const second = arguments[0];
+        for (let i = first; i <= second; i++) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
